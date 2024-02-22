735624c37520c0257757346e5018635c0fe00d11
SofieAastrup <sofie.s.aastrup@gmail.com>
2024-02-22 16:21:25 +0100

    added template code

--- Galaga/Game.cs
+++ Galaga/Game.cs
@@ -4,22 +4,36 @@ using DIKUArcade.Input;
 
 namespace Galaga
 {
-    public class Game : DIKUGame
-    {
+    public class Game : DIKUGame, IGameEventProcessor {
         public Game(WindowArgs windowArgs) : base(windowArgs) {
             // TODO: Set key event handler (inherited window field of DIKUGame class)
+
         }
 
-        //private void KeyHandler(KeyboardAction action, KeyboardKey key) {} // TODO: Outcomment
 
-        public override void Render()
-        {
+        public override void Render(){
             throw new System.NotImplementedException("Galaga game has nothing to render yet.");
         }
 
-        public override void Update()
-        {
+        public override void Update(){
             throw new System.NotImplementedException("Galaga game has no entities to update yet.");
         }
+
+        private void KeyPress(KeyboardKey key) {
+            // TODO: Close window if escape is pressed
+            // TODO: switch on key string and set the player's move direction
+        }
+
+        private void KeyRelease(KeyboardKey key) {
+            // TODO: switch on key string and disable the player's move direction
+        }
+
+        private void KeyHandler(KeyboardAction action, KeyboardKey key) {
+            // TODO: Switch on KeyBoardAction and call proper method
+         }
+
+        public void ProcessEvent(GameEvent gameEvent) {
+            // Leave this empty for now
+    }
     }
 }
---
+++ Galaga/PlayerShot.cs
@@ -0,0 +1,12 @@
+using DIKUArcade.Entities;
+using DIKUArcade.Graphics;
+using DIKUArcade.Math;
+
+namespace Galaga;
+public class PlayerShot : Entity {
+
+    public PlayerShot(Vec2F position, IBaseImage image){
+
+    }
+    
+}
